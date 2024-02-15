decfa1aecc52308b3a70f1965f71798ddad0b8ab
Кирилл Попов <diplomatgmg@gmail.com>
2024-02-15 15:52:55 +0300

    Добавлено правило для упаковки css

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ export default {
         exclude: /node_modules/,
         use: 'ts-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
